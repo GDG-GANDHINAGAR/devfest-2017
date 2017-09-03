1a4a49ae40ade704fc0c46e9078498d612386b1e
parth181195 <parthjansari@outlook.com>
2017-09-03 17:37:17 +0530

    added browser sync stream

--- gulpfile.js
+++ gulpfile.js
@@ -16,49 +16,53 @@ var gutil = require('gulp-util');
 
 
 var onError = function(err) {
-    notify.onError({
-        title: "Gulp error in " + err.plugin,
-        message: err.toString()
-    })(err);
-    beeper(3);
-    this.emit('end');
-    gutil.log(gutil.colors.red(err));
+  notify.onError({
+    title: "Gulp error in " + err.plugin,
+    message: err.toString()
+  })(err);
+  beeper(3);
+  this.emit('end');
+  gutil.log(gutil.colors.red(err));
 };
 //this block will give error in red fonts instade of stopping on error
 
 gulp.task('styles', function() {
-    gulp.src('assets/sass/style.sass')
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(sourcemaps.init())
-        .pipe(sass({ indentedSyntax: true }))
-        .pipe(autoprefixer({
-            browsers: ['last 5 versions'],
-            cascade: false
-        }))
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write('.'))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('./assets/css'));
+  gulp.src('assets/sass/style.sass')
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(sourcemaps.init())
+    .pipe(sass({ indentedSyntax: true }))
+    .pipe(autoprefixer({
+      browsers: ['last 5 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('.'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+
 });
 //styles task to compile sass and build sass sourcemaps
 
 
 gulp.task('default', function() {
-    gulp.start('styles', 'watch');
+  gulp.start('styles', 'watch');
 });
 
 gulp.task('watch', function() {
-    gulp.watch('assets/sass/**/*', ['styles']);
-    gulp.watch('assets/js/*.js', browserSync.reload);
-    gulp.watch('assets/img/*.*', browserSync.reload);
-    gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('assets/sass/**/*', ['styles']);
+  gulp.watch('assets/js/*.js', browserSync.reload);
+  gulp.watch('assets/img/*.*', browserSync.reload);
+  gulp.watch('./*.html', browserSync.reload);
 
-    // init server
-    browserSync.init({
-        server: {
-            proxy: "local.build",
-            baseDir: "./"
-        }
-    });
+  // init server
+  browserSync.init({
+    server: {
+      proxy: "local.build",
+      baseDir: "./"
+    }
+  });
 
 });
