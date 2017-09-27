f39d534a2e2529ec8c2e922ff4334720b803b1f0
parth181195 <parthjansari@outlook.com>
2017-09-27 18:00:48 +0530

    added uid logo

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,17 @@ $(window).scroll(function() {
     $("nav#nav").removeClass('shadow')
   }
 })
+
+function myMap() {
+  var mapOptions = {
+    center: new google.maps.LatLng(23.2026592, 72.5825373),
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.MAP
+  }
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var marker = new google.maps.Marker({
+    position: mapOptions.center,
+    map: map,
+    title: 'Hello World!'
+  });
+}
